e1ac7dad0b30dea141621f4d06a4122d5f8b0197
manisharaghwa <er.amnisharaghwa@gmail.com>
2018-09-28 02:33:09 -0700

    first commit

---
+++ assignment2.py
@@ -0,0 +1,29 @@
+#Q.1- Print anything you want on screen.
+print("i am manisha")
+
+#Q.2- Join two strings using '+'. 
+m="Acad"+"View”
+print(m)
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen. 
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+z=input("Enter the value of z:")
+print(x,y,z)
+#Q.4- Print “Let’s get started” on screen.
+print("\"Let's get started\" ")
+#Q.5- Print the following values using placeholders. 
+#s=”Acadview” 
+#course=”Python” 
+#fees=5000
+s="Acadview"
+course="python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+#Q.6- Find the area of circle 
+#pi = 3.14 
+#Take radius as input from user 
+#Print the area of circle
+pi=3.14
+
+
